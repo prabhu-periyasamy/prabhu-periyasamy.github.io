4ee19e8bcc66796eff9d05ccee956039f87c1a52
prabhu periyasamy <prabhu.periyasamy@freshworks.com>
2022-12-11 21:54:58 +0530

    Update submit.js

--- js/submit.js
+++ js/submit.js
@@ -14,13 +14,7 @@ $("#sub-btn-edm").on("click", function () {
     var aa = document.getElementById("form-section"),
         form = document.getElementById("webinar-form")
     
-    //$("#sub-btn-edm").prop("disabled", true);
-    
-//     aa.classList.toggle("toogle-success")
-//     setTimeout(() => {
-//         aa.classList.toggle("toogle-success");
-//         form.reset();
-//     }, 3000);
+    $("#sub-btn-edm").prop("disabled", true);
     
     $.ajax({
         type: 'GET',
@@ -31,7 +25,7 @@ $("#sub-btn-edm").on("click", function () {
         dataType: 'json',
         encode: true,
         success: function(o) {
-            //$("#sub-btn-edm").prop("disabled", false);
+            $("#sub-btn-edm").prop("disabled", false);
         }
     });
 });
